61bc6dd8b98314e97f48413bba14eacf0cd4d220
Karunyareddy966 <153362657+Karunyareddy966@users.noreply.github.com>
2025-10-15 17:48:00 +0530

    Update code1.java

--- JavaExercises/Datatype_program/Operational_programs/code1.java
+++ JavaExercises/Datatype_program/Operational_programs/code1.java
@@ -1,1 +1,29 @@
+public class Operator_Program2 {
+    public static void main(String[] args) {
+
+        int a = 15, b = 4;
+
+        // Arithmetic Operators
+        System.out.println("=== Arithmetic Operators ===");
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Relational Operators
+        System.out.println("\n=== Relational Operators ===");
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+
+        // Logical Operators
+        boolean x = true, y = false;
+        System.out.println("\n=== Logical Operators ===");
+        System.out.println("x && y : " + (x && y));
+        System.out.println("x || y : " + (x || y));
+        System.out.println("!x : " + (!x));
+    }
+}
 
