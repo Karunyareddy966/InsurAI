4bbb4247e88f3bbbd7e74f9d697edf797c730bb9
Karunyareddy966 <153362657+Karunyareddy966@users.noreply.github.com>
2025-10-15 17:41:55 +0530

    Update code2.java

--- JavaExercises/Datatype_program/Datatype_programs/code2.java
+++ JavaExercises/Datatype_program/Datatype_programs/code2.java
@@ -1,1 +1,12 @@
-
+// OperatorPrograms/ArithmeticExample.java
+public class ArithmeticExample {
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 4;
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y)); // integer division
+        System.out.println("x % y = " + (x % y));
+    }
+}
