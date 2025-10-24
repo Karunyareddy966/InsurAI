59a237a8829a1c767dc547de4140ea7d438a1ef1
Karunyareddy966 <153362657+Karunyareddy966@users.noreply.github.com>
2025-10-24 21:37:53 +0530

    Create dashboard.js

---
+++ React/dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="page">
+      <h2>Admin Dashboard</h2>
+      <p>Manage users, agents, plans, and appointments here.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
