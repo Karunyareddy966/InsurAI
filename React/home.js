503e85af4d2b12c45d67713fc5b3ffdbe115258e
Karunyareddy966 <153362657+Karunyareddy966@users.noreply.github.com>
2025-10-24 21:36:22 +0530

    Create home.js

---
+++ React/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="page">
+      <h1>Welcome to Insurance Portal</h1>
+      <p>Your one-stop platform for all types of insurance — health, car, shop, and more.</p>
+    </div>
+  );
+}
+
+export default Home;
